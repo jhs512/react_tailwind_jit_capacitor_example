ffd6e769772d9d317f161cc38fb665b0e7bb633d
jhs512 <jangka512@gmail.com>
2021-09-30 13:37:40 +0900

    classNames 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import classNames from "classnames/bind";
+import { useState } from "react";
+
 function App() {
-  return (
-    <div className="App bg-red-500">
-      ㅋㅋ
-    </div>
-  );
+  const [actived, setActived] = useState(false)
+  return <div onClick={() => setActived(!actived)} className={classNames("w-[100px]", "h-10", {"bg-red-500": actived})}>ㅋㅋ</div>;
 }
 
 export default App;
